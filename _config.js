d9939f0b26dc1823a4ea895aef7ece8cb0598a15
iqbalyarkhan <iqbal.khan@utdallas.edu>
2023-02-11 00:12:48 -0600

    cleanup

--- _config.js
+++ _config.js
@@ -14,7 +14,7 @@ module.exports = {
     // If the file does not exist, it is replaced by a random image.
 
   /** Home > Bio information*/
-  comment: 'full stack engineer 🧑🏽‍💻',
+  comment: '🧑🏽‍💻',
   name: 'User Name',
   company: '',
   location: 'USA',
